237814e3281efa665731a4efce87a834e97a5fd9
azozulyuk <kvitochka101010@gmail.com>
2026-03-19 00:28:04 -0400

    Refactor MenuItem and Menu display methods
    Updated MenuItem and Menu classes to improve functionality and display methods.

--- ww/Menu.cpp
+++ ww/Menu.cpp
@@ -36,8 +36,8 @@ namespace seneca {
     MenuItem::MenuItem(const char* content,
         unsigned int indent,
         unsigned int indentSize,
-        unsigned int row) {
-
+        unsigned int row)
+    {
         text = nullptr;
         ind = indent;
         indSize = indentSize;
@@ -53,43 +53,47 @@ namespace seneca {
         delete[] text;
     }
 
-    void MenuItem::display() const {
+    MenuItem::operator bool() const {
+        return text != nullptr;
+    }
 
-        if (text) {
+    ostream& MenuItem::display(ostream& ostr) const {
 
-            
-            for (unsigned int i = 0; i < ind * indSize; i++) {
-                cout << " ";
-            }
-
-            if (indexOfRow > 0) {
-                cout.width(2);
-                cout << indexOfRow << "- ";
-            }
-            else if (indexOfRow == 0 && text != nullptr && text[0] != '>') {
-                cout.width(2);
-                cout << 0 << "- ";
-            }
-
-            cout << text;
+        if (!(*this)) {
+            return ostr; 
         }
+
+        for (unsigned int i = 0; i < ind * indSize; i++)
+            ostr << ' ';
+
+        if (indexOfRow > 0) {
+            ostr.width(2);
+            ostr << indexOfRow << "- ";
+        }
+        else if (indexOfRow == 0) {
+            ostr.width(2);
+            ostr << 0 << "- ";
+        }
+
+        ostr << text;
+
+        return ostr;
     }
 
     Menu::Menu(const char* title,
         const char* exitOption,
         unsigned int indent,
         unsigned int indentSize)
-        : h1(title, indent, indentSize, 0),    
-        exit(exitOption, indent, indentSize, 0), 
-        intput("> ", indent, indentSize, 0)   
+        : h1(title, indent, indentSize, -1),
+          exit(exitOption, indent, indentSize, 0),
+          intput("> ", indent, indentSize, -1)
     {
         ind = indent;
         indSize = indentSize;
         items = 0;
 
-        for (unsigned int i = 0; i < MaximumNumberOfMenuItems; i++) {
+        for (unsigned int i = 0; i < MaximumNumberOfMenuItems; i++)
             attributes[i] = nullptr;
-        }
     }
 
     Menu::~Menu() {
@@ -114,28 +118,24 @@ namespace seneca {
 
     size_t Menu::select() const {
 
-    if (h1.text) {
-        h1.display();
-        cout << endl;
+        if (h1)
+            h1.display(cout) << endl;
+
+        for (unsigned int i = 0; i < items; i++) {
+            attributes[i]->display(cout) << endl;
+        }
+
+        exit.display(cout) << endl;
+
+        intput.display(cout);
+
+        return ut.getInt(0, items);
     }
-
-    for (unsigned int i = 0; i < items; i++) {
-        attributes[i]->display();
-        cout << endl;
-    }
-
-    exit.display();   
-
-    intput.display(); 
-
-    return ut.getInt(0, items);
-}
 
     size_t operator<<(ostream& ostr, const Menu& m) {
 
-        if (&ostr == &cout) {
+        if (&ostr == &cout)
             return m.select();
-        }
 
         return 0;
     }
