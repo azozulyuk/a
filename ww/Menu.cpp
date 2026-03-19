955b327a07a1dc54c4becc242c69731b2a7bb4fa
azozulyuk <kvitochka101010@gmail.com>
2026-03-19 00:47:53 -0400

    Revise citation and sources in Menu.cpp
    Updated citation and sources information in Menu.cpp.

--- ww/Menu.cpp
+++ ww/Menu.cpp
@@ -1,4 +1,4 @@
-﻿/* Citation and Sources...
+/* Citation and Sources...
 -----------------------------------------------------------
 Final Project Milestone 2:2
 Module: 2
@@ -14,7 +14,7 @@ Revision History
 Date      Reason
 2026/03/9  Preliminary release
 2026/03/10  Debugged code
-2026/03/16  Ready to go
+2026/03/18 Ready to go
 -----------------------------------------------------------
 I have done all the coding by myself and only copied the code
 that my professor provided to complete my workshops and assignments.
@@ -56,31 +56,29 @@ namespace seneca {
 
         if (!text) return;
 
-        // indentation
         for (unsigned int i = 0; i < ind * indSize; i++) {
             cout << " ";
         }
 
-        // numbering
         if (indexOfRow > 0) {
-            cout.width(2);
-            cout << indexOfRow << "- ";
+            if (indexOfRow < 10)
+                cout << " " << indexOfRow << "- ";
+            else
+                cout << indexOfRow << "- ";
         }
-        else if (indexOfRow == 0 && text[0] != '>') {
-            cout.width(2);
-            cout << 0 << "- ";
+        else if (indexOfRow == 0 && text[0] != 'T' && text[0] != 'M' && text[0] != '>') {
+            cout << " 0- ";
         }
 
         cout << text;
     }
-
     Menu::Menu(const char* title,
         const char* exitOption,
         unsigned int indent,
         unsigned int indentSize)
         : h1(title, indent, indentSize, 0),
-          exit(exitOption, indent, indentSize, 0),
-          intput("> ", indent, indentSize, 0)
+        exit(exitOption, indent, indentSize, 0),
+        intput("> ", indent, indentSize, 0)
     {
         ind = indent;
         indSize = indentSize;
@@ -110,32 +108,30 @@ namespace seneca {
 
     size_t Menu::select() const {
 
-        // title
         if (h1.text) {
             h1.display();
             cout << endl;
         }
 
-        // items
         for (unsigned int i = 0; i < items; i++) {
             attributes[i]->display();
             cout << endl;
         }
 
-        // exit
         exit.display();
         cout << endl;
 
-        // prompt (NO endl here!!)
         intput.display();
 
         return ut.getInt(0, items);
     }
 
     size_t operator<<(ostream& ostr, const Menu& m) {
+
         if (&ostr == &cout) {
             return m.select();
         }
+
         return 0;
     }
 
